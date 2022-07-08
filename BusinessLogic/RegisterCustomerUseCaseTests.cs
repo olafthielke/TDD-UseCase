757403bd4cad1a1d349120630cd1e90a40d65752
Olaf Thielke <olaf.thielke@gmail.com>
2022-07-08 13:04:36 +1200

    Call Register() without customer - Red (Compile)

--- BusinessLogic/RegisterCustomerUseCaseTests.cs
+++ BusinessLogic/RegisterCustomerUseCaseTests.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System;
+using FluentAssertions;
+using Xunit;
 
 namespace BusinessLogic
 {
@@ -10,20 +12,33 @@ namespace BusinessLogic
             var useCase = new RegisterCustomerUseCase();
         }
 
+        //[Fact]
+        //public void Can_Call_Register()
+        //{
+        //    var useCase = new RegisterCustomerUseCase();
+        //    useCase.Register();
+        //}
+
         [Fact]
-        public void Can_Call_Register()
+        public void Given_No_Customer_When_Call_Register_Then_Throw_MissingCustomer_Exception()
         {
             var useCase = new RegisterCustomerUseCase();
-            useCase.Register();
+            Action register = () => useCase.Register(null);
+            register.Should().ThrowExactly<MissingCustomer>();
         }
     }
 
 
     public class RegisterCustomerUseCase
     {
-        public void Register()
+        public void Register(object customer)
         {
 
         }
+    }
+
+    public class MissingCustomer : Exception
+    {
+
     }
 }
