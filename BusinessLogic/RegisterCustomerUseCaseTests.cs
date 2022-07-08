44dfa50b6deab8600c60b47a97a8257b3da8098f
Olaf Thielke <olaf.thielke@gmail.com>
2022-07-08 16:26:35 +1200

    Call Register() without customer.LastName throws Exception - Red (Compile)

--- BusinessLogic/RegisterCustomerUseCaseTests.cs
+++ BusinessLogic/RegisterCustomerUseCaseTests.cs
@@ -28,6 +28,14 @@ namespace BusinessLogic
             register.Should().ThrowExactly<MissingFirstName>()
                 .WithMessage("Missing first name.");
         }
+
+        [Fact]
+        public void Given_No_LastName_When_Call_Register_Then_Throw_MissingLastName_Exception()
+        {
+            var useCase = new RegisterCustomerUseCase();
+            Action register = () => useCase.Register(new Customer("Fred", null));
+            register.Should().ThrowExactly<MissingLastName>();
+        }
     }
 
 
