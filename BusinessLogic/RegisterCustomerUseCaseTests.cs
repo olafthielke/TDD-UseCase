a8c38fa076836be1722b1a3e5898a487c00e4206
Olaf Thielke <olaf.thielke@gmail.com>
2022-07-08 12:47:44 +1200

    Can instantiate RegisterCustomerUseCase - Red (Compile)

---
+++ BusinessLogic/RegisterCustomerUseCaseTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace BusinessLogic
+{
+    public class RegisterCustomerUseCaseTests
+    {
+        [Fact]
+        public void Can_Instantiate_UseCase()
+        {
+            var useCase = new RegisterCustomerUseCase();
+        }
+    }
+}
