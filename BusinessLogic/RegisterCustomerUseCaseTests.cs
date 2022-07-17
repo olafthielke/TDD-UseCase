7b5e7b28a6642f169b3e84a021ce3b57818f3ff4
Olaf Thielke <olaf.thielke@gmail.com>
2022-07-17 12:25:52 +1200

    When call Register() for another new customer Then save customer to repository - Red (Runtime)

--- BusinessLogic/RegisterCustomerUseCaseTests.cs
+++ BusinessLogic/RegisterCustomerUseCaseTests.cs
@@ -87,6 +87,7 @@ namespace BusinessLogic
 
         [Theory]
         [InlineData("Fred", "Flintstone", "fred@flintstones.net")]
+        [InlineData("Barney", "Rubble", "barney@rubbles.rock")]
         public void Given_New_Customer_When_Call_Register_Then_Save_Customer_To_Repository(string firstName,
             string lastName, string emailAddress)
         {
