3d6a462461cdb1a2ab531dcc9e232d8ceb83659d
Olaf Thielke <olaf.thielke@gmail.com>
2022-07-18 13:48:14 +1200

    When call Register() and customer already exists Then throw Exception with message - Green

--- BusinessLogic/Exceptions/DuplicateCustomerEmailAddress.cs
+++ BusinessLogic/Exceptions/DuplicateCustomerEmailAddress.cs
@@ -4,6 +4,8 @@ namespace BusinessLogic.Exceptions
 {
     public class DuplicateCustomerEmailAddress : Exception
     {
-
+        public DuplicateCustomerEmailAddress()
+            : base("Customer with email address 'fred@flintstones.net' already exists.")
+        { }
     }
 }
