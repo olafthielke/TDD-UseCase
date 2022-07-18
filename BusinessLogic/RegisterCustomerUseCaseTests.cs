13e275acd80ca95fcca2fe1d66413150a812166a
Olaf Thielke <olaf.thielke@gmail.com>
2022-07-18 13:25:48 +1200

    When call Register() try get customer by email address from repository - Green (Forward-looking test)

--- BusinessLogic/RegisterCustomerUseCaseTests.cs
+++ BusinessLogic/RegisterCustomerUseCaseTests.cs
@@ -62,6 +62,7 @@ namespace BusinessLogic
         [Theory]
         [InlineData("Fred", "Flintstone", "fred@flintstones.net")]
         [InlineData("Barney", "Rubble", "barney@rubbles.rock")]
+        [InlineData("Wilma", "Flintstone", "wilma@flintstones.net")]
         public void When_Call_Register_Then_Try_Lookup_Customer_By_EmailAddress(string firstName,
             string lastName, string emailAddress)
         {
